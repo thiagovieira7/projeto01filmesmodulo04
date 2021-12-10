4447add8bb80e3e9f8428f38c2b270e5b457a200
Thiago Vieira <thiagovieira7@outlook.com>
2021-12-10 02:24:34 -0300

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'projeto01filmesmodulo04/tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
